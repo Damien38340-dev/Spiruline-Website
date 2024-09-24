44d51c08f254a7989310ba3ca82807de5928c20b
Damien LOBATO <damien.lobato@Digital-Grenoble.local>
2024-09-24 15:29:14 +0200

    Ajout composants SuppliersList et SuppliersMap

---
+++ Supplier/src/components/SuppliersList.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    name: "SuppliersList"
+  }
+}
+</script>
+
+<template>
+<h1> Suppliers List </h1>
+</template>
+
+<style scoped>
+h1{
+  font-size: 3rem;
+  text-align: center;
+  color: #00bd7e;
+}
+</style>
---
+++ Supplier/src/components/SuppliersMap.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    name: "SuppliersMap"
+  }
+}
+</script>
+
+<template>
+  <h1> Suppliers Map </h1>
+</template>
+
+<style scoped>
+h1{
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: cadetblue;
+}
+</style>
